b83e76b62fd1528ddf2364521a47d7f24a59c26e
Alejandro Gallego <alegallego@MacBook-Pro-de-Alejandro.local>
2024-10-22 22:14:48 -0500

    first commit

---
+++ page-object/BasePage.ts
@@ -0,0 +1,28 @@
+import { Page } from "@playwright/test";
+
+export default abstract class BasePage {
+  readonly page: Page;
+  public path: string;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+  setPath(path: string) {
+    this.path = path;
+  }
+  async navigate() {
+    await this.page.goto(this.path);
+  }
+
+  async clickElementByText(text) {
+    await this.page.getByText(text).click();
+  }
+
+  async clickElementByPlaceholder(placeholder) {
+    await this.page.getByPlaceholder(placeholder).click();
+  }
+
+  async clickElementByLabel(label) {
+    await this.page.getByLabel(label).click();
+  }
+}
